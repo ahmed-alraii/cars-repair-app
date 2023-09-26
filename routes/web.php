d3c23a9dc72502bbf0dcde75903f23533e9389b1
ahmed-alraii <alameer8377@gmail.com>
2023-09-26 16:41:26 +0400

    refactor web routes

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,32 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::redirect('/', app()->getLocale());
-Route::group(['prefix' => '{language}'], function () {
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+Route::group(['prefix' => '{language}' ,'middleware' => 'auth'], function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('welcome');
 
-    Route::resource('/users', UserController::class)->middleware('auth');
-    Route::resource('/_cars', CarController::class)->middleware('auth');
-    Route::resource('/containers', ContainerController::class)->middleware('auth');
-    Route::resource('/bills', BillController::class)->middleware('auth');
-    Route::resource('/bill_types', BillTypeController::class)->middleware('auth');
+    Route::post('/mark_all_read', [OrderController::class, 'markAllAsRead'])->name('mark_all_read');
+    Route::get('/bill_report', [ReportController::class, 'billReport'])->name('bill_report');
+    Route::post('/bill_report', [ReportController::class, 'generateBillReport'])->name('generate_bill_report');
+    Route::get('/bill_export', [ReportController::class, 'billExport'])->name('bill_export');
 
-    Route::post('/mark_all_read', [OrderController::class, 'markAllAsRead'])->name('mark_all_read')->middleware(['auth']);
-    Route::get('/bill_report', [ReportController::class, 'billReport'])->name('bill_report')->middleware(['auth']);
-    Route::post('/bill_report', [ReportController::class, 'generateBillReport'])->name('generate_bill_report')->middleware(['auth']);
-    Route::get('/bill_export', [ReportController::class, 'billExport'])->name('bill_export')->middleware(['auth']);
+    Route::resource('/users', UserController::class);
+    Route::resource('/_cars', CarController::class);
+    Route::resource('/containers', ContainerController::class);
+    Route::resource('/bills', BillController::class);
+    Route::resource('/bill_types', BillTypeController::class);
 
+
+});
+
+Route::group(['prefix' => '{language}'], function () {
 //    Route::middleware('auth')->group(function () {
 //        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 //        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 //    });
-
     require __DIR__ . '/auth.php';
 });
 
