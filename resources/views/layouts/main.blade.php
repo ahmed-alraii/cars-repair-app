96e70297fffc813fc8e0aa3cb07c02c98735a1f1
ahmed-alraii <alameer8377@gmail.com>
2023-09-30 09:40:59 +0400

    main layout for prod

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,7 @@
     <link href="{{ asset('admin/vendor/css/dataTable.scroller.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href={{ asset('build/assets/app-e5178921.css') }}>
 
 
     <link rel="stylesheet" href={{ asset('admin/vendor/css/myStyle.css') }}>
