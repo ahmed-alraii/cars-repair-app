b7d9e53b931d372c77ca433fab332244e72500f0
ahmed-alraii <alameer8377@gmail.com>
2023-09-30 10:52:18 +0400

    main layout for prod

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -20,7 +20,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
 
 
-    <link rel="stylesheet" href={{ asset('build/assets/app-e5178921.css') }}>
 
     <link rel="stylesheet" href="{{asset('admin/vendor/css/demo/myStyle.css')}}">
 </head>
