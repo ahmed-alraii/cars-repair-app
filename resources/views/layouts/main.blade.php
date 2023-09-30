c73dc443d96b8439880a867e36ab1a38cfd1b073
ahmed-alraii <alameer8377@gmail.com>
2023-09-30 10:57:54 +0400

    main layout for dev

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,7 +36,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 
     <link rel="stylesheet" href={{ asset('build/assets/app-e5178921.css') }}>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
 
